cce73cd69dcbe75e306dc6972fe3fa37b1d48b1e
Al Hussain <ahussain@winsystems.com>
2024-01-31 15:06:15 -0600

    added ttymxc script
    added ttymxc script

---
+++ ttymxc1_self_loopback.py
@@ -0,0 +1,8 @@
+import serial
+
+ser = serial.Serial('/dev/ttymxc1', baudrate=115200, timeout=1)  # open first serial port
+print(ser.portstr)       # check which port was really used
+ser.write(b'hello')      # write a string
+msg = ser.read(size=100) #read the content of the input buffer until you get 100 byte or a timeout event
+print(msg) #print the content you might need to decode it print(decode(msg))
+ser.close()
