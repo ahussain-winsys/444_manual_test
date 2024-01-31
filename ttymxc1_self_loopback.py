3b4b193d4d045d99f6054ffe943c5cc12333d3d7
Al Hussain <ahussain@winsystems.com>
2024-01-31 15:10:05 -0600

    Update ttymxc1_self_loopback.py
    updated p/f message

--- ttymxc1_self_loopback.py
+++ ttymxc1_self_loopback.py
@@ -1,8 +1,15 @@
 import serial
 
+tx_msg = (b'hello')
+
 ser = serial.Serial('/dev/ttymxc1', baudrate=115200, timeout=1)  # open first serial port
 print(ser.portstr)       # check which port was really used
-ser.write(b'hello')      # write a string
+ser.write(tx_msg)      # write a string
 msg = ser.read(size=100) #read the content of the input buffer until you get 100 byte or a timeout event
 print(msg) #print the content you might need to decode it print(decode(msg))
 ser.close()
+
+if tx_msg is msg:
+    print("PASS")
+else:
+    print("FAIL")
