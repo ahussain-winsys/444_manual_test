366670642ed52c579bf6f2c5519dcb61fe90a247
Al Hussain <ahussain@winsystems.com>
2024-01-31 15:18:06 -0600

    updated string formatting in ttymxc script

--- ttymxc0_self_loopback.py
+++ ttymxc0_self_loopback.py
@@ -1,8 +1,16 @@
 import serial
 
+tx_msg = (b'hello')
+
 ser = serial.Serial('/dev/ttymxc0', baudrate=115200, timeout=1)  # open first serial port
 print(ser.portstr)       # check which port was really used
-ser.write(b'hello')      # write a string
+ser.write(tx_msg)      # write a string
+print("Sending - " + str(tx_msg))
 msg = ser.read(size=100) #read the content of the input buffer until you get 100 byte or a timeout event
-print(msg) #print the content you might need to decode it print(decode(msg))
+print("Received - " + str(msg)) #print the content you might need to decode it print(decode(msg))
 ser.close()
+
+if str(tx_msg) == str(msg):
+    print("PASS")
+else:
+    print("FAIL")
--- ttymxc1_self_loopback.py
+++ ttymxc1_self_loopback.py
@@ -5,11 +5,12 @@ tx_msg = (b'hello')
 ser = serial.Serial('/dev/ttymxc1', baudrate=115200, timeout=1)  # open first serial port
 print(ser.portstr)       # check which port was really used
 ser.write(tx_msg)      # write a string
+print("Sending - " + str(tx_msg))
 msg = ser.read(size=100) #read the content of the input buffer until you get 100 byte or a timeout event
-print(msg) #print the content you might need to decode it print(decode(msg))
+print("Received - " + str(msg)) #print the content you might need to decode it print(decode(msg))
 ser.close()
 
-if tx_msg is msg:
+if str(tx_msg) == str(msg):
     print("PASS")
 else:
     print("FAIL")
